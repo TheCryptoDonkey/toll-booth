aa6ee600b9add2700f7fa4c5018fddb8b1422222
The Crypto Donkey <cryptofire365@gmail.com>
2026-02-27 17:36:55 +0000

    fix: export EventHandler type from barrel

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // src/index.ts
 export { lightningGate } from './middleware.js'
+export type { EventHandler } from './middleware.js'
 export { mintMacaroon, verifyMacaroon, parseCaveats } from './macaroon.js'
 export { CreditMeter } from './meter.js'
 export { FreeTier } from './free-tier.js'
